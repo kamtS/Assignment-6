b6a037adbc654040c5a2ae94a2f81d56b5202bc8
kamts <dan91johnson@gmail.com>
2016-01-19 13:21:27 +0000

    final touches

--- js/index.js
+++ js/index.js
@@ -9,43 +9,64 @@ $('document').ready(function() {
 		    src: 'images/austin.jpg'},
 		{
 		    name: ['la', 'los angeles', 'lax'],
-		    src: 'images/la.jpg'
-		},
+		    src: 'images/la.jpg'},
 		{
 		    name: ['sf', 'san fransisco', 'san fran'],
-		    src: 'images/sf.jpg'
-		},
+		    src: 'images/sf.jpg'},
 		{
 		    name: ['sydney', 'syd'],
-		    src: 'images/sydney.jpg'
-		}
+		    src: 'images/sydney.jpg'}
     ];
     
     $('#submit-btn').click(function(e) {
         e.preventDefault();
-        console.log('button clicked');
+       // console.log('button clicked');
      
        var userInput = $('#city-type').val();
        userInput = userInput.toLowerCase().trim();
        
        
         images.forEach(function(element, index) {
-            console.log(element, index);
+           // console.log(element, index);
         
         
-            console.log(element.src);
+            //console.log(element.src);
             element.name.forEach(function(element1, index1) {
-                console.log(element1, index1);
+              //  console.log(element1, index1);
                 
                 if(userInput === element1) {
                     $('body').css('background-image', 'url(' + element.src + ')');
                     
-                    console.log('new image');
+                //    console.log('new image');
                 }
             });
         
         });
     
+          /*  version 2
+            
+            images.forEach(imagesObject);
+            
+            function imagesObject(element, index) {
+                console.log(element, index);
+                
+                element.name.forEach(cityName);
+                // body...
+            }
+            
+            function cityName(element1, index1) {
+                console.log(element1, index1);
+                
+                console.log(images(index).src);
+                console.log(element.src);
+                
+                if(userInput === element1) {
+                    $('body').css('background-image', 'url(' + element.src + ')');
+           
+          
+            }
+        });
+                 end of version 2 */
     });
 
 });
