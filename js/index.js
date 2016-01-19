1f50e43c8a83dbd04591200461204b6e3b706efd
kamts <dan91johnson@gmail.com>
2016-01-19 05:57:50 +0000

    image array

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
 
 $('document').ready(function(); {
-    
+    var images = [ 
+        { 
+            name: 'new york', 'ny', 'NYC';
+		    src: '../images/nyc.jpg';}
+		{ 
+		    name: 'austin';
+		    src: '../images/austin.jpg';}
+		{
+		    name: 'la', 'los angeles', 'lax';
+		    src: '../images/la.jpg';
+		}
+		{
+		    name: 'sf', 'san fransisco', 'san fran';
+		    src: '../images/sf.jpg';
+		}
+		{
+		    name: 'sydney', 'syd';
+		    src: '../images/sydney.jpg'
+		}
+];
     
     
 }
+
+/* 
+
+*/
